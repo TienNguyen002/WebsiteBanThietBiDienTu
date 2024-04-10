a7fe79ff72332545be7d5e2cefa9bcc65b054a92
TIenNguyenHoangNhat <tien.nguyenho@titancorp.vn>
2024-04-10 21:16:04 +0700

    Category Banner

--- src/client/src/Pages/homePage/HomePage.jsx
+++ src/client/src/Pages/homePage/HomePage.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react";
 import Header from "../../Components/header/Header";
 import Banner from "../../Components/banner/Banner";
-import FlashSale from "../../Components/flashSale/FlashSale";
-import Product from "../../Components/product/Product";
-import Footer from "../../Components/footer/Footer";
 
 const HomePage = () => {
   useEffect(() => {
@@ -13,10 +10,7 @@ const HomePage = () => {
   return (
     <div>
       <Header />
-      {/* <Banner />
-      <FlashSale />
-      <Product />
-      <Footer /> */}
+      <Banner />
     </div>
   );
 };
